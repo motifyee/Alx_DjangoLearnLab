281a5c7cba481515a6300920309a9e5c5a21a8b5
mrt <iquixe@gmail.com>
2024-08-18 07:33:57 +0300

    fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from relationship_app.models import Book, Library
 
-# Create your views here.
+
+def all_books(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
+
+
+class LibraryView(ListView):
+    model = Library
+
+    def get_context_data(self, **kwargs):
+        library_name = ""
+
+        context = super().get_context_data(**kwargs)
+
+        library = Library.objects.get(name=library_name)
+        context['library'] = library
