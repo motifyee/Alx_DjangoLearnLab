14db7045d4c463441a645422c2cc3de3ff78758a
mrt <iquixe@gmail.com>
2024-08-18 06:43:50 +0300

    fix

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -18,3 +18,5 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=30)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+# return self.name
