fbf66eb9d9b21aaefa6ceada7ecdc0b85d127cce
mrt <iquixe@gmail.com>
2024-08-18 06:46:23 +0300

    fix

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,5 +1,5 @@
 from relationship_app.models import Book, Librarian, Library
 
 author_books = Book.objects.get(author="author name")
-library_books = Library.objects.get(name=library_name)
+library_books = Library.objects.get(name=library_name, kwargs="books.all()")
 library_librarian = Book.objects.get(library="lib")
