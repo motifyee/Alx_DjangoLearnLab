34ada615d857be96acf8e700daab268d172149d4
mrt <iquixe@gmail.com>
2024-08-25 09:36:15 +0300

    .

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('books/', all_books, name='books'),
     path('library', LibraryDetailView.as_view(), name='library'),
     path('login', LoginView.as_view(template_name="login.html"), name='login'),
-    path('logout', LogoutView.as_view(
-        template_name="logout.html"), name='logout'),
+    path('logout', LogoutView.as_view(template_name="logout.html"), name='logout'),
 ]
 
 # views.register
