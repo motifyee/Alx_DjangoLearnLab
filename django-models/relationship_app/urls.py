2cd7fe6d686676bbda1e2401a8ec89abdb22b5d2
mrt <iquixe@gmail.com>
2024-08-25 09:25:05 +0300

    .

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import all_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', all_books, name='books'),
-    path('library', LibraryView.as_view(), name='library')
+    path('library', LibraryDetailView.as_view(), name='library')
 ]
